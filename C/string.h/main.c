e528c7ac3e7abfabfeb12cba19fbf86a78c1dd56
Ahmed Fathi <ahmed.fathi.aziz@gmail.com>
2023-09-08 04:44:39 +0300

    update main.c

--- C/string.h/main.c
+++ C/string.h/main.c
@@ -3,12 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-char s1[101] = "--Hello - World! - World!";
-char s2[101] = "";
-
 int main() {
-  unsigned int n = fstrxfrm(s2, s1, 100);
-  printf("n = %i \n", n);
-  printf("%s \n", s2);
   return 0;
 }
